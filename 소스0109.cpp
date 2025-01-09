04fd957234b74a19acd83856b76093d22236de62
lactea0125 <hoony2526@gmail.com>
2025-01-09 16:53:54 +0900

    Add files via upload

---
+++ 소스0109.cpp
@@ -0,0 +1,27 @@
+//#include<stdio.h>
+//
+//void main()
+//{
+//	printf("100+100");
+//	printf("\n");
+//	printf("%d", 100 + 100);
+//	printf("\n");
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("æ»≥Á«œºŒø‰");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("æ»≥Á«œººø‰\n");
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	printf("æ»≥Á«œººø‰\næ»≥Á«œººø‰\næ»≥Á«œººø‰\n");
+	return 0;
+}
