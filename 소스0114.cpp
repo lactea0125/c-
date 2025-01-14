f60138ca62ad256a633ba86619cdbde8fa93bc1c
lactea0125 <hoony2526@gmail.com>
2025-01-14 16:52:46 +0900

    Add files via upload

---
+++ 소스0114.cpp
@@ -0,0 +1,80 @@
+//#include<stdio.h>
+//
+//void main()
+//{
+//	printf("100+100");
+//	printf("\n");
+//	printf("%d", 100 + 100);
+//	printf("\n");
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0; 
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n");
+//		return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n안녕하세요\n안녕하세요\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	printf("안녕하세요\n");
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개\n", apple);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드 %d개\n", keyboard);
+//	printf("마우스 %d\n", mouse);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int coin1 = 500;
+	int coin2 = 1000;
+	printf("%d+%d=%d\n", coin1, coin2, coin1 + coin2);
+	return 0;
+}
