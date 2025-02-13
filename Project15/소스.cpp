40bab9dced340fee9f74584a3512af4cb1b7387c
lactea0125 <hoony2526@gmail.com>
2025-02-13 16:53:17 +0900

    Add files via upload

---
+++ Project15/소스.cpp
@@ -0,0 +1,178 @@
+//#include<stdio.h>
+//void main()
+//{
+//	int a = 200;
+//	if (a < 100)
+//	{
+//		printf("100보다 작군요..\n");
+//		printf("거짓이므로 앞의 문장은 안보이겠죠?\n");
+//
+//	}
+//	printf("프로그램 끝!");
+//}
+//#define _CRT_SECURE_NO_WARINGS
+//#include<stdio.h>
+//void main()
+//{
+//	int a;
+//	printf("정수를 입력하세요:");
+//	scanf("%d",&a);
+//
+//
+//	if (a % 2 == 0)
+//	{
+//		printf("짝수를 입력했군요..\n");
+//	}
+//	else
+//	{
+//		printf("홀수를 입력했군요..\n");
+//
+//	}
+//}
+//#include<stdio.h>
+//void main()
+//{
+//	int a = 75;
+//	if (a < 50)
+//	{
+//		if (a < 100)
+//		{
+//			printf("50보다 크고 100보다 작군요..\n");
+//
+//		}
+//		else {
+//			printf("와~~100보다크군요..\n");
+//
+//		}
+//	}
+//	else
+//	{
+//		printf("에게~ 50보다 작군요..\n");
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int apple = 0;
+//	if (apple == 10)
+//	{
+//		printf("apple은 10개 있습니다.\n");
+//	}
+//	else if (apple == 0)
+//	{
+//		printf("apple은 하나도 없습니다.\n");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int apple = 15;
+//	if (!apple)
+//	{
+//		printf("apple은 하나도 없습니다.\n");
+//	}
+//	else
+//	{
+//		printf("apple은 0개가 아닙니다.\n");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("값을 입력하세요:");
+//	scanf("%d", &a);
+//	if (a < 0)
+//	{
+//		printf("음수");
+//	}
+//	else if (a > 0)
+//	{
+//		printf("양수");
+//	}
+//	else
+//	{
+//		printf("0 입니다");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("값을 입력하세요:");
+//	scanf("%d", &a);
+//	if (10 <=a && a<=99)
+//	{
+//		printf("input은 두 자리 숫자입니다");
+//	}
+//	else
+//	{
+//		printf("input은 두 자리 숫자가 아닙니다");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int height = 170;
+//	if (height > 150)
+//	{
+//		printf("키가 150 이상입니다.\n");
+//	}
+//	if (height > 160)
+//	{
+//		printf("키가 160 이상입니다.\n");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int height = 170;
+//	if (height > 150)
+//	{
+//		printf("키가 150 이상 입니다\n");
+//    }
+//	else if (height > 160)
+//	{
+//		printf("키가 160 이상입니다.\n");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("나이 입력:");
+//	scanf("%d", &a);
+//	if (a > 12)
+//	{
+//		printf("12세 관람가\n");
+//	}
+//	if (a > 15)
+//	{
+//		printf("15세 관람가\n");
+//	}
+//	if(a>0)
+//	{
+//		printf("전체 관람가\n");
+//	}
+//}
+#include<stdio.h>
+int main()
+{
+	float a;
+	printf("문자하나:");
+	scanf("%f", &a);
+	if (37 <= a && 5c >= a )
+    {
+		printf("대문자");
+	}
+	if (97 <= a && 123 >= a)
+	{
+		printf("소문자");
+	}
+}
