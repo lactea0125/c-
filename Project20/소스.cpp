7ee740eef866c3162123b92b6ad7ce8e095218dc
lactea0125 <hoony2526@gmail.com>
2025-02-27 19:44:11 +0900

    Add files via upload

---
+++ Project20/소스.cpp
@@ -0,0 +1,111 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int count = 3;
+//	while (count)
+//	{
+//		if (count == 1)
+//		{
+//			break;
+//		}
+//		printf("현재 count : %d\n", count);
+//		printf("count가 0이 아니면 반복\n");
+//		count--;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("Hello World!");
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	char alphabet;
+//	while (1) {
+//		printf("알파벳을 입력해주세요(다른 것입력시 다시 입력):");
+//		scanf("%c", &alphabet);
+//		if (alphabet >= 'a' && alphabet <= 'z')
+//		{
+//			printf("소문자를 입력하셨습니다.\n");
+//		}
+//		else if (alphabet >= 'A' && alphabet <= 'Z')
+//		{
+//			printf("대문자를 입력 하셨습니다.\n");
+//		}
+//		else {
+//			continue;
+//		}
+//
+//		printf("입력한 값 : %c\n", alphabet);
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int number, total = 0;
+//	while (1)
+//	{
+//		printf("양수를 입력해주세요(0입력시 종료);");
+//		scanf("%d", &number);
+//		if (number == 0)
+//		{
+//			break;
+//		}
+//		else if (number < 0)
+//		{
+//			continue;
+//		}
+//		total = total + number;
+//		printf("%d\n", total);
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{	int number = 1;
+//	int multi=1;
+//	while(1)
+//	{
+//		multi = multi * number;
+//		
+//		if (multi >= 50000)
+//		{
+//			break;
+//		}	
+//		number++;
+//	
+//    }
+//	printf("%d\n", number);
+//	int a =1;
+//	int f = 1;
+//	while (1)
+//	{
+//		f = f * a;
+//		a++;
+//		if (a > 10)
+//		{
+//			break;
+//		}
+//	}
+//	printf("%d\n", f);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	char a;
+	while (1)
+	{
+	 (a >= 'a' && a <= 'z')
+		
+			printf("%d\n", a);
+			a++;
+	
+	if ('f')
+		{
+			break;
+		}
+	}
+}
